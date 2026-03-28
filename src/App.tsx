0a92b18108f17c0b82587d1b8e582e281e562ba1
neathcmd <ksdf71464@gmail.com>
2026-03-28 08:58:21 +0700

    init react app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Welcome to the Student Life Admin Portal
+      </h1>
+    </div>
+  );
+};
+
+export default App;
